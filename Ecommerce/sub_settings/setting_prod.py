639dd130eb2681bc304218dc01d5b1d9134d1804
PyWendi <rakotondranaivogilbert21@gmail.com>
2024-03-22 18:09:34 +0300

    Correction of the env secret key config on prod setting

--- Ecommerce/sub_settings/setting_prod.py
+++ Ecommerce/sub_settings/setting_prod.py
@@ -32,7 +32,7 @@ SIMPLE_JWT = {
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": False,
     "ALGORITHM": "HS256",
-    "SIGNING_KEY": env["SECRET_KEY"],
+    "SIGNING_KEY": env.get("SECRET_KEY"),
     "VERIFYING_KEY": "",
     "AUDIENCE": None,
     "ISSUER": None,
